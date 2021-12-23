aaac8ef2bff6146b7a0097fd09e124484d18c36a
Taha Berk YEŞİLALİOĞLU <63355948+BerkZorbey@users.noreply.github.com>
2021-12-23 17:55:58 +0300

    Create mario-task.c

---
+++ mario-task.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h = get_int("1-8arası bir yükseklik giriniz:\n");
+    
+    
+        if(h>0 && h<9){
+        for(int i=0;i<h;i++){
+            for(int k=h-i;k>1;k--){
+                printf(" ");
+            }
+            for(int j=0;j<i+1;j++){
+                   printf("#");
+                   
+            }
+          printf("\n");  
+        }
+        
+        }
+       
+    
+}
